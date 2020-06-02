47cf538ca768248ec83c8fc24f599020e2d988a7
sprockmonty <sprockmonty@gmail.com>
2020-06-02 05:33:05 +0100

    added error section

--- weaveGen.jl
+++ weaveGen.jl
@@ -2,5 +2,5 @@ using Weave
 
 weave("thesis.jmd", out_path="pdf", doctype = "md2pdf")
 run(`makeindex ./pdf/thesis.nlo -s nomencl.ist -o ./pdf/thesis.nls`)
-run(`bibtex ./pdf/references.bib`)
+run(`bibtex ./pdf/thesis`)
 weave("thesis.jmd", out_path="pdf", doctype = "md2pdf")
